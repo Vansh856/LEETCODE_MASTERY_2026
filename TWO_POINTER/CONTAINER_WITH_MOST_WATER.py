7db66e223fbea7b76ed57d795309145bf1a62584
Vansh Shan <vanshshan5@gmail.com>
2026-01-10 06:58:48 +0530

    Add files via upload

---
+++ TWO_POINTER/CONTAINER_WITH_MOST_WATER.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r=len(height)-1
+        maxa=0
+        while l<r:
+            area=min(height[l],height[r])*(r-l)
+            maxa=max(maxa,area)
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return maxa
